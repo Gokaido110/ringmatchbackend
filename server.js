ce9d548f0a2c7322066ebd57de57068e18a1ec88
dhavalJB <74847516+dhavalJB@users.noreply.github.com>
2024-12-17 13:28:20 +0530

    Youre

--- server.js
+++ server.js
@@ -46,13 +46,17 @@ app.use("/user", userRoutes);
 app.use("/payments", paymentRoutes);
 app.use("/transactions", transactionRoutes);
 
-mongoose.connect(`${process.env.MONGODB_URL}`).then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(
-      `Succesfuly connected to db ${process.env.MONGODB_URL} and app running on port ${process.env.PORT}`
-    );
+mongoose.connect(`${process.env.MONGODB_URL}`)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `Successfully connected to db ${process.env.MONGODB_URL} and app running on port ${process.env.PORT}`
+      );
+    });
+  })
+  .catch(err => {
+    console.error("Error connecting to the database:", err.message);
+    console.log(err);
   });
-}).catch(err => {
-  console.log("error while connectiong to db")
-});
+
 
